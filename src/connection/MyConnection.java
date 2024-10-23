4248bb1aa43c7a0db958a518a5134d525bff1212
khuong <khuongbb28@gmail.com>
2024-10-23 15:41:27 +0700

    second

--- src/connection/MyConnection.java
+++ src/connection/MyConnection.java
@@ -7,18 +7,18 @@ import javax.swing.JOptionPane;
  * @author huynh
  */
 public class MyConnection {
-    public static final String username = "";
+    public static final String username = "root";
     public static final String passWord = "";
-    public static final String url = " jdbc:mysql://locasthost:3306/convenience_system";
+    public static final String url = "jdbc:mysql://localhost:3306/convenience_system";
     public static Connection con = null;
     
     public static Connection getConnection(){
         try {
-            Class.forName("com.myspl.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url, username, passWord);
         } catch (Exception ex) {
            JOptionPane.showMessageDialog(null, ""+ex, "", JOptionPane.WARNING_MESSAGE);
-        }
+        }              
         return con;
     }
 }
